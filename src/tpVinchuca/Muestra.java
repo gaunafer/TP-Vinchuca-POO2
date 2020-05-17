90282b2e8eb7af7b417acf2563356a8eea0cd8ac
Natalia <natalia.caporale@uala.com.ar>
2020-05-17 14:56:43 -0300

    propuesta de enum en la clasificacion de fotos

--- src/tpVinchuca/Muestra.java
+++ src/tpVinchuca/Muestra.java
@@ -1,5 +1,17 @@
 package tpVinchuca;
 
 public class Muestra {
-
+	
+	private String clasificacion; //= ClasificacionDeFoto.VINCHUCA_INFESTANS;
+	
+	
+	public Muestra(Persona p, Ubicacion u, ClasificacionDeFoto c) {
+		this.clasificacion=c.getDescripcion();
+	}
+	
+	//public String getClasificacion() {
+	//return clasificacion.getDescripcion();
+	
+	//}
+	
 }
