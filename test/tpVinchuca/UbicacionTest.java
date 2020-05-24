24244abb1bedc152e800981fb95552c65d73ec6e
Cintuza <cinbiblio@yahoo.com.ar>
2020-05-23 21:53:58 -0300

    versión inicial

--- test/tpVinchuca/UbicacionTest.java
+++ test/tpVinchuca/UbicacionTest.java
@@ -1,5 +1,116 @@
 package tpVinchuca;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 public class UbicacionTest {
 
+	//ubicaciones posibles
+	private Ubicacion ubicacion1;
+	private Ubicacion ubicacion2;
+	private Ubicacion ubicacion3;
+	private Ubicacion ubicacion4;
+	
+	//ubicaciones imposibles, solo se usan en un test cada uno, quizas no deberian estar en el setUp
+	private Ubicacion ubicacion5;
+	private Ubicacion ubicacion6;
+	private Ubicacion ubicacion7;
+	private Ubicacion ubicacion8;
+
+	
+	
+	@BeforeEach
+	public void setUp() {
+		//primer double latitud, segundo double longitud
+		
+		//ubicaciones posibles
+		ubicacion1 = new Ubicacion(-34.627911, -58.380361); //Constitucion
+		ubicacion2 = new Ubicacion(-34.777857, -58.308091); //Solano
+		ubicacion3 = new Ubicacion(-34.795691, -58.287063); //Varela
+		ubicacion4 = new Ubicacion(-34.822964, -58.111839); //Parque Pereyra
+	}
+	
+	
+	
+	
+	@Test
+	public void testCreacionUbicacion1() {
+		Double latUbicacion1 = ubicacion1.getLatitud();
+		Double longUbicacion1 = ubicacion1.getLongitud();
+		
+		assertEquals(-34.627911, latUbicacion1);
+		assertEquals(-58.380361, longUbicacion1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreacionUbicacion5() {
+		ubicacion5 = new Ubicacion(-94.603066, -58.443407);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreacionUbicacion6() {
+		ubicacion6 = new Ubicacion(94.627911, -58.380361);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreacionUbicacion7() {
+		ubicacion7 = new Ubicacion(-34.603066, -198.443407);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCreacionUbicacion8() {
+		ubicacion8 = new Ubicacion(-34.627911, 198.380361);
+	}
+	
+	public void testCalcularDistancia() {
+		//execute
+		Double distanciaConUbicacion2 = ubicacion1.calcularDistancia(ubicacion2); //resultado: 17.95
+		Double distanciaConUbicacion3 = ubicacion1.calcularDistancia(ubicacion3); //resultado: 20.53
+		Double distanciaConUbicacion4 = ubicacion1.calcularDistancia(ubicacion4); //resultado: 32.78
+		
+		//verify
+		//ver margen de error -> se agrega el punto decimal a lo ultimo dentro del parentesis del assert
+		assertEquals(17.95, distanciaConUbicacion2, 0.02);
+		assertEquals(20.53, distanciaConUbicacion3, 0.02);
+		assertEquals(32.78, distanciaConUbicacion4, 0.02);
+	}
+	
+	
+	public void testUbicacionesAMenosDe() {
+		// setUp
+		ArrayList<Ubicacion> ubicaciones = new ArrayList<Ubicacion>();
+		ubicaciones.add(ubicacion2);
+		ubicaciones.add(ubicacion3);
+		ubicaciones.add(ubicacion4);
+		
+		//execute
+		//ubicaciones a menos de 30km
+		ArrayList<Ubicacion> ubicacionesAMenosDe30 = ubicacion1.ubicacionesAMenosDe(30d, ubicaciones);
+		int sizeUbicacionesAMenosDe30 = ubicacionesAMenosDe30.size();
+		
+		//ubicaciones a menos de 10km
+		ArrayList<Ubicacion> ubicacionesAMenosDe10 = ubicacion1.ubicacionesAMenosDe(10d, ubicaciones);
+		int sizeUbicacionesAMenosDe10 = ubicacionesAMenosDe10.size();
+		
+		//verify
+		assertEquals(2, sizeUbicacionesAMenosDe30);
+		assertEquals(0, sizeUbicacionesAMenosDe10);
+	}
+	
+	public void testMuentrasAMenosDe() {
+		
+		//muestra mockito?
+		//la lista deberia tenerlas o acceder la clase Ubicacion
+		
+		ArrayList<Muestra> muestrasAMenosDe15km = muestrasAMenosDe(15, muestra): List;
+		int sizeMuestrasAMenosDe15km = muestrasAMenosDe15km.size(); 
+		
+		assertEquals(, sizeMuestrasAMenosDe15km);
+	}
+	
 }
--- test/tpVinchuca/ZonaDeCoberturaTest.java
+++ test/tpVinchuca/ZonaDeCoberturaTest.java
@@ -1,5 +1,85 @@
 package tpVinchuca;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 public class ZonaDeCoberturaTest {
+	
+	private ZonaDeCobertura zona1;
+	private ZonaDeCobertura zona2;
+	private ZonaDeCobertura zona3;
+	private ZonaDeCobertura zona4;
+	
+	//aca imagino que habria que usar mockito no?
+	private Ubicacion epicentro1;
+	private Ubicacion epicentro2;
+	private Ubicacion epicentro3;
+	private Ubicacion epicentro4;
+	
+	
+	public void setUp() {
+		epicentro1 = new Ubicacion(-34.688910, -58.314271);
+		epicentro2 = new Ubicacion(-34.811389, -58.274080);
+		epicentro3 = new Ubicacion(-34.688910, -58.314271);
+		epicentro4 = new Ubicacion(-34.751968, -58.234205);
+		
+		// epicentros en estaciones
+		zona1 = new ZonaDeCobertura(epicentro1, 2d, "Costanera Dominico Wilde"); // acceso parque santo domingo
+		zona2 = new ZonaDeCobertura(epicentro2, 4d, "Varela"); //epicentro en estacion
+		zona3 = new ZonaDeCobertura(epicentro3, 3.5d, "Solano"); //epicentro en 844 y donato alvarez
+		zona4 = new ZonaDeCobertura(epicentro4, 4d, "Ezpeleta"); //epicentro en estacion
+	}
+	
+	
+	//no testeo epicentro porque seria testear Ubicacion, que ya esta testeada
+	//no se me ocurrieron excepciones... probar que no se puede cargar un radio negativo/vacio quizas?
+	public void testCreacionZonaDeCobertura() {
+		//en teoria no habria que crear un metodo para poder hacer correr un test asi que no se si estan bien estos gets...
+		Double radio = zona1.getRadio();
+		String nombre = zona1.getNombre();
+		
+		//reformular el assert? pero contra que variable? el nombre quizas?
+		assertEquals(2, radio);
+		assertEquals("Costanera Dominico Wilde", nombre);
+	}
+	
+	
+	
+	//metodo privado seSolapaCon(ZonaDeCobertura) sin testear
+	
+	
+	//ejemplo de zona que se solapa con otras dos zonas
+	public void testZonasConLasQueSeSolapa() {
+		//setUp
+		ArrayList<ZonaDeCobertura> listaDeZonas = new ArrayList();
+		listaDeZonas.add(zona1); //distancia Dominico-Varela: 14.03
+		listaDeZonas.add(zona3); //distancia Solano-Varela: 4.91
+		listaDeZonas.add(zona4); //distancia Ezpeleta-Varela: 7.48
+		
+		//execute
+		ArrayList<ZonaDeCobertura> listaDeZonasConLasQueSeSolapa = zona2.zonasConLasQueSeSolapa(listaDeZonas);
+		Integer cantDeZonasConLasQueSeSolapa = listaDeZonasConLasQueSeSolapa.size(); 
+		
+		//verify
+		assertEquals(2, cantDeZonasConLasQueSeSolapa);
+	}
+	
+
+	//ejemplo de zona que no se solapa con ninguna zona
+	public void testZonasConLasQueSeSolapa() {
+		//setUp
+		ArrayList<ZonaDeCobertura> listaDeZonas = new ArrayList();
+		listaDeZonas.add(zona2); //distancia Varela-Dominico: 14.03
+		listaDeZonas.add(zona3); //distancia Solano-Dominico: 10.37
+		listaDeZonas.add(zona4); //distancia Ezpeleta-Dominico: 10.09
+		
+		//execute
+		ArrayList<ZonaDeCobertura> listaDeZonasConLasQueSeSolapa = zona1.zonasConLasQueSeSolapa(listaDeZonas);
+		Integer cantDeZonasConLasQueSeSolapa = listaDeZonasConLasQueSeSolapa.size(); 
+		
+		//verify
+		assertEquals(0, cantDeZonasConLasQueSeSolapa);
+	}
 
 }
