7838d593f19fe0b5679fa0ede6b636c0cda89721
Cintuza <cinbiblio@yahoo.com.ar>
2020-05-27 00:54:42 -0300

    Desarrollo metodos calcularDistancia y ubicacionesAMenosDe

--- src/tpVinchuca/Ubicacion.java
+++ src/tpVinchuca/Ubicacion.java
@@ -1,5 +1,8 @@
 package tpVinchuca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ubicacion {
 
 	private Double latitud;
@@ -32,6 +35,36 @@ public class Ubicacion {
 		 } else {
 			this.longitud = longitud;
 	     }
+	}
+
+	public Double calcularDistancia(Ubicacion otraUbicacion) {
+		Double lat1 = getLatitud();
+		Double lng1 = getLongitud();
+		
+		Double lat2 = otraUbicacion.getLatitud();
+		Double lng2 = otraUbicacion.getLongitud();
+		
+		double radioTierra = 6371;//en kilómetros  
+        double dLat = Math.toRadians(lat2 - lat1);  
+        double dLng = Math.toRadians(lng2 - lng1);  
+        double sindLat = Math.sin(dLat / 2);  
+        double sindLng = Math.sin(dLng / 2);  
+        double va1 = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)  
+                * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));  
+        double va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));  
+        double distancia = radioTierra * va2;  
+   
+        return distancia;
+	}
+
+	public List<Ubicacion> ubicacionesAMenosDe(double distancia, List<Ubicacion> ubicaciones) {
+		List<Ubicacion> ubicacionesAMenosDe = new ArrayList<Ubicacion>();
+		for (Ubicacion ubicacion : ubicaciones) {
+			if (this.calcularDistancia(ubicacion) < distancia) {
+				ubicacionesAMenosDe.add(ubicacion);
+			}
+		}
+		return ubicacionesAMenosDe;
 	}
 
 	
