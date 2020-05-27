d60177b40b10979de45bf61474ce2c7d8a7cbe7f
Cintuza <cinbiblio@yahoo.com.ar>
2020-05-26 21:53:04 -0300

    Agregado de constructor, getter y setters

--- src/tpVinchuca/Ubicacion.java
+++ src/tpVinchuca/Ubicacion.java
@@ -2,4 +2,38 @@ package tpVinchuca;
 
 public class Ubicacion {
 
+	private Double latitud;
+	private Double longitud;
+	
+	public Ubicacion(Double latitud, Double longitud) {
+		setLatitud(latitud);
+		setLongitud(longitud);
+	}
+
+	public Double getLatitud() {
+		return latitud;
+	}
+
+	public Double getLongitud() {
+		return longitud;
+	}
+
+	public void setLatitud(Double latitud) {
+		 if ((latitud < -90 || latitud > 90)) {
+	          throw new IllegalArgumentException("Ubicacion inexistente. La latitud debe tener un valor entre -90 y 90");
+	       } else {
+	    	   this.latitud = latitud;
+	       }
+	}
+
+	public void setLongitud(Double longitud) {
+		 if (longitud < -180 || longitud > 180) {
+			throw new IllegalArgumentException("Ubicacion inexistente. La longitud debe tener un valor entre -180 y 180");
+		 } else {
+			this.longitud = longitud;
+	     }
+	}
+
+	
+	
 }
