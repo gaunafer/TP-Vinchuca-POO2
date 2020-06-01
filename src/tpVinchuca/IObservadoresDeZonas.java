2a2e6c2c17d3cd4212d3dbad282cfcf9cf451d4f
Cintuza <cinbiblio@yahoo.com.ar>
2020-06-01 00:49:19 -0300

    Version inicial

---
+++ src/tpVinchuca/IObservadoresDeZonas.java
@@ -0,0 +1,8 @@
+package tpVinchuca;
+
+public interface IObservadoresDeZonas {
+
+	public void updateCreacionMuestra(Muestra muestra, ZonaDeCobertura zona);
+	public void updateValidacionMuestra(Muestra muestra, ZonaDeCobertura zona);
+	
+}
