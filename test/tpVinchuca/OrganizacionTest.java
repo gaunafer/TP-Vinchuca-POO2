00b6a48a5463519611ec79ba522c0a91af97a191
Cintuza <cinbiblio@yahoo.com.ar>
2020-06-01 00:52:29 -0300

    Creacion de test para metodos relacionados con observer

--- test/tpVinchuca/OrganizacionTest.java
+++ test/tpVinchuca/OrganizacionTest.java
@@ -1,5 +1,24 @@
 package tpVinchuca;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.*;
+
 public class OrganizacionTest {
+	
+	// Testea el constructor de la clase Organizacion
+	@Test
+	public void testCreacionOrganizacion() {
+		Ubicacion ubicFlech = mock(Ubicacion.class);
+		Organizacion flech = new Organizacion(TipoDeOrganizacion.SANITARIA, ubicFlech, 25);
+		
+		Ubicacion ubicacion = flech.getUbicacion();
+		TipoDeOrganizacion tipoDeOrganizacion = flech.getTipoDeOrganizacion();
+		Integer cantDeTrabajadores = flech.getCantidadDeTrabajadores();
+		
+		assertEquals(ubicFlech, ubicacion);
+		assertEquals("SANITARIA", tipoDeOrganizacion.toString());
+		assertEquals(25, cantDeTrabajadores);
+	}
 
 }
