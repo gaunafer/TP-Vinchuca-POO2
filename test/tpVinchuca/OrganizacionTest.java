70ceaa03d28127faa5e0568da30af8b48ca609c3
Cintuza <cinbiblio@yahoo.com.ar>
2020-06-01 12:45:45 -0300

    Agregado de tests de update

--- test/tpVinchuca/OrganizacionTest.java
+++ test/tpVinchuca/OrganizacionTest.java
@@ -5,12 +5,27 @@ import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.*;
 
 public class OrganizacionTest {
+
+	private Ubicacion ubicFlech;
+	private Organizacion flech;
+
+	private IFuncionalidadExterna funcionalidad;
+	private Muestra muestra;
+	private ZonaDeCobertura zona;
+	
+	@BeforeEach
+	public void setUp() {
+		ubicFlech = mock(Ubicacion.class);
+		flech = new Organizacion(TipoDeOrganizacion.SANITARIA, ubicFlech, 25);
+		
+		funcionalidad = mock(IFuncionalidadExterna.class);
+		muestra = mock(Muestra.class);
+		zona = mock(ZonaDeCobertura.class);
+	}
 	
 	// Testea el constructor de la clase Organizacion
 	@Test
 	public void testCreacionOrganizacion() {
-		Ubicacion ubicFlech = mock(Ubicacion.class);
-		Organizacion flech = new Organizacion(TipoDeOrganizacion.SANITARIA, ubicFlech, 25);
 		
 		Ubicacion ubicacion = flech.getUbicacion();
 		TipoDeOrganizacion tipoDeOrganizacion = flech.getTipoDeOrganizacion();
@@ -19,6 +34,26 @@ public class OrganizacionTest {
 		assertEquals(ubicFlech, ubicacion);
 		assertEquals("SANITARIA", tipoDeOrganizacion.toString());
 		assertEquals(25, cantDeTrabajadores);
+	}
+	
+	// Chequea que el metodo updateCreacionMuestra llame correctamente al nuevoEvento de la funcionalidadExterna
+	@Test
+	public void testUpdateCreacionMuestra() {
+		IFuncionalidadExterna funcionalidad = mock(IFuncionalidadExterna.class);
+		flech.setFuncionalidadCreacionDeMuestra(funcionalidad);
+		
+		flech.updateCreacionMuestra(muestra, zona);
+		verify(funcionalidad).nuevoEvento(flech, zona, muestra);
+	}
+	
+	// Chequea que el metodo updateValidacionMuestra llame correctamente al nuevoEvento de la funcionalidadExterna
+	@Test
+	public void testUpdateValidacionMuestra() {
+		IFuncionalidadExterna funcionalidad = mock(IFuncionalidadExterna.class);
+		flech.setFuncionalidadValidacionDeMuestra(funcionalidad);
+		
+		flech.updateValidacionMuestra(muestra, zona);
+		verify(funcionalidad).nuevoEvento(flech, zona, muestra);
 	}
 
 }
