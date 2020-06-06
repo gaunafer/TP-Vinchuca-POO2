88a02f00b328042c635c338e9f5f989b590c0f1e
Cintuza <cinbiblio@yahoo.com.ar>
2020-06-06 10:51:48 -0300

    Eliminacion metodo esMuestraDeZona

--- src/tpVinchuca/ClaseX.java
+++ src/tpVinchuca/ClaseX.java
@@ -1,34 +1,45 @@
 package tpVinchuca;
 
 import java.util.*;
-import java.util.function.BooleanSupplier;
 
 public class ClaseX {
 	
 	private List<ZonaDeCobertura> zonasQueContienenLaMuestra;
 
+	/**
+	 * Creacion de clase
+	 * Esta clase contiene las zonas de cobertura que abarcan la ubicacion
+	 * de una determinada muestra.
+	 * Su funcion es avisarle a dichas zonas que la muestra ha sido validada
+	 */
 	public ClaseX() {
 		zonasQueContienenLaMuestra = new ArrayList<ZonaDeCobertura>();
 	}
 
+	/**
+	 * Agrega una zona de cobertura a la lista de zonas
+	 * @param zona
+	 */
 	public void agregarZonaDeCobertura(ZonaDeCobertura zona) {
 		zonasQueContienenLaMuestra.add(zona);
 	}
 
-	public Boolean esZonaDeMuestra(ZonaDeCobertura zona) {
-		return zonasQueContienenLaMuestra.contains(zona);
-	}
-
+	/**
+	 * Elimina una zona de cobertura de la lista
+	 * @param zona Zona a ser eliminada de la lista
+	 */
 	public void eliminarZonaDeCobertura(ZonaDeCobertura zona) {
 		zonasQueContienenLaMuestra.remove(zona);
 	}
 
+	/**
+	 * Le avisa a las zonas de cobertura que una muestra ha sido validada en su zona
+	 * @param muestra Muestra que ha sido validada
+	 */
 	public void muestraValidada(Muestra muestra) {
 		for (ZonaDeCobertura zonaDeCobertura : zonasQueContienenLaMuestra) {
 			zonaDeCobertura.muestraValidida(muestra);
 		}		
 	}
-	
-	
 	
 }
--- test/tpVinchuca/ClaseXTest.java
+++ test/tpVinchuca/ClaseXTest.java
@@ -1,6 +1,5 @@
 package tpVinchuca;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,34 +10,47 @@ class ClaseXTest {
 	private ClaseX claseX;
 	
 	private ZonaDeCobertura solano;
+	private ZonaDeCobertura varela;
+	private Muestra muestra;
 	
 	@BeforeEach
 	public void setUp() {
 		claseX = new ClaseX();
+		
 		solano = mock(ZonaDeCobertura.class);
-
+		varela = mock(ZonaDeCobertura.class);
+		muestra = mock(Muestra.class);
+	}
+	
+	// Verifica:
+	// - el agregado de las zonas de cobertura a la lista
+	// - la activacion del metodo muestraValidada para cada zona de cobertura de la lista
+	@Test
+	public void testSeValidaUnaMuestraYSeAvisaALasZonasDeCobertura() {
 		claseX.agregarZonaDeCobertura(solano);
-	}
-
-	@Test
-	public void testAgregarZonaDeCobertura() {
+		claseX.agregarZonaDeCobertura(varela);
 		
-		assertTrue(claseX.esZonaDeMuestra(solano));
-	}
-	
-	@Test
-	public void testEliminarZonaDeCobertura() {
-		claseX.eliminarZonaDeCobertura(solano);
-		
-		assertFalse(claseX.esZonaDeMuestra(solano));
-	}
-	
-	@Test
-	public void testMuestraValidada() {
-		Muestra muestra = mock(Muestra.class);
 		claseX.muestraValidada(muestra);
 		
 		verify(solano).muestraValidida(muestra);
+		verify(varela).muestraValidida(muestra);
+	}
+
+	// Verifica:
+	// - la eliminacion de zonas de cobertura de la lista
+	// - al no haber zonas de cobertura en la lista, no se les avisa ante la validacion de una muestra
+	@Test
+	public void testSeValidaUnaMuestraPeroNoHayZonasDeCobertura() {
+		claseX.agregarZonaDeCobertura(solano);
+		claseX.agregarZonaDeCobertura(varela);
+		
+		claseX.eliminarZonaDeCobertura(solano);
+		claseX.eliminarZonaDeCobertura(varela);
+		
+		claseX.muestraValidada(muestra);
+		
+		verify(solano, never()).muestraValidida(muestra);
+		verify(varela, never()).muestraValidida(muestra);
 	}
 
 }
