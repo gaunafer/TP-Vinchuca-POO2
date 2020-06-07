86a01d08dd2deb8782e86e42f54384c46ea1dc42
Cintuza <cinbiblio@yahoo.com.ar>
2020-06-07 20:35:10 -0300

    Agregado metodo asignarZona

--- src/tpVinchuca/Muestra.java
+++ src/tpVinchuca/Muestra.java
@@ -9,10 +9,10 @@ public class Muestra {
 	private List<Votacion> votaciones;
 	private Participante participante;
 	private NivelDeValidacion nivelDeValidacion;
-	private List<ZonaDeCobertura> zonasDeCobertura;
 	private Imagen imagen;
 	private ResultadoDeMuestra veredicto;
 	private Ubicacion ubicacion;
+	private ClaseX claseX;
 	
 	public Muestra(Imagen imagen, Participante participante, ResultadoDeMuestra veredicto, Ubicacion ubicacion) {
 		this.participante = participante;
@@ -21,7 +21,8 @@ public class Muestra {
 		this.veredicto = veredicto; 
 		this.ubicacion = ubicacion;
 		this.inicializarEstado();
-		 this.votaciones = new ArrayList<Votacion>();
+		this.votaciones = new ArrayList<Votacion>();
+		this.claseX = new ClaseX();
 	}
 	
 	
@@ -76,12 +77,25 @@ public class Muestra {
 	public Imagen getImagen() {
 		return this.imagen;
 	}
+	
+	public ClaseX getClaseX() {
+		return this.claseX;
+	}
+	
 	public void addVotacion(Votacion votacion) {
 		this.votaciones.add(votacion);		
 	}
 
 	public Participante getParticipante() {
 		return participante;
+	}
+
+	/**
+	 * Asigna la zona a la lista de zonas de la ClaseX a las que pertenece la muestra
+	 * @param zona ZonaDeCobertura
+	 */
+	public void asignarZona(ZonaDeCobertura zona) {
+		getClaseX().agregarZonaDeCobertura(zona);
 	}
 
 
