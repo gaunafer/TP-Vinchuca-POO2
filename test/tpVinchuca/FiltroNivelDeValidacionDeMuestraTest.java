4f27d8f2bbc19887d3684a667c521e3bf7e280de
Natalia <natalia.caporale@uala.com.ar>
2020-06-09 13:01:11 -0300

    FValidacionMuestra

--- test/tpVinchuca/FiltroNivelDeValidacionDeMuestraTest.java
+++ test/tpVinchuca/FiltroNivelDeValidacionDeMuestraTest.java
@@ -1,5 +1,20 @@
 package tpVinchuca;
 
-public class FiltroNivelDeValidacionDeMuestraTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class FiltroNivelDeValidacionDeMuestraTest {
+	 private  Filtro filtroNivelDeValidacion;
+	 
+	 @BeforeEach
+	 public void setUp() {
+		 filtroNivelDeValidacion = new FiltroNivelValidacionMuestra();
+		 
+	 }
+	 
+	 
+	 @Test
+	 public void seTraenTodasLasMuestrasQueCorrespondenAlNivelDeValidacionDado() {
+		 
+	 }
 }
