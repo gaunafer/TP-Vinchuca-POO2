6881edaf7ee41c4e063c6e6ca916783d1760726e
Fernando gauna <gaunafer@gmail.com>
2020-06-10 23:00:47 -0300

    comentarios participante

--- src/tpVinchuca/Participante.java
+++ src/tpVinchuca/Participante.java
@@ -12,26 +12,41 @@ public class Participante {
 		this.nivelDeConocimiento = nivelDeConocimiento;	
 	}
 
+	/**
+	 * Retorna el alias del participante
+	 */
 	public String getAlias() {
 		return alias;
 	}
 
-
+	/**
+	 * Retorna el nivel de conocimiento del participante
+	 */
 	public String getNivelDeConocimiento() {
 		this.actualizarEstado();
 		
 		 return this.nivelDeConocimiento.getNivelDeConocimiento();
 	}
 	
+    /**
+     * Setea el nivel de conocimiento
+     * @param nivelDeConocimiento
+     */
     public void setNivelDeConocimiento(NivelDeConocimiento nivelDeConocimiento) {
 		this.nivelDeConocimiento = nivelDeConocimiento;
 		
 	}
 
+	/**
+	 * Delega en estado la actualizacion del estado del participante al estado correspondiente
+	 */
 	public void actualizarEstado() {
 		this.nivelDeConocimiento.verificarEstado(this);
 	}	
 	
+	/**
+	 * Redefine equals para busquedas realizar busquedas por igualdad
+	 */
 	@Override
 	public boolean equals(Object participante) {
 		Participante otroParticipante = (Participante)participante;
