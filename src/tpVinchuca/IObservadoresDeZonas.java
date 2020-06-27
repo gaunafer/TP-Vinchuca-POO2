86a634c813aae26146c6ce16887424aac16e5f68
Cintuza <cinbiblio@yahoo.com.ar>
2020-06-26 22:13:07 -0300

    agregado metodo interesEnZona

--- src/tpVinchuca/IObservadoresDeZonas.java
+++ src/tpVinchuca/IObservadoresDeZonas.java
@@ -4,5 +4,6 @@ public interface IObservadoresDeZonas {
 
 	public void updateCreacionMuestra(Muestra muestra, ZonaDeCobertura zona);
 	public void updateValidacionMuestra(Muestra muestra, ZonaDeCobertura zona);
+	public void interesEnZona(ZonaDeCobertura zona);
 	
 }
